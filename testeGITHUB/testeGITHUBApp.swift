1b5b0b56aa280b6b09317948296f8dd3c89692f8
Leon Sanchez <leonpsanchez@gmail.com>
2021-03-24 23:06:28 -0300

    Initial Commit

---
+++ testeGITHUB/testeGITHUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeGITHUBApp.swift
+//  testeGITHUB
+//
+//  Created by Leon Sanchez on 24/03/21.
+//
+
+import SwiftUI
+
+@main
+struct testeGITHUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
